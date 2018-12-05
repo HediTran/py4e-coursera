040c4353a08e7678a1592aa2682ed3e9720542f6
Thanh Giang Tran <43576846+HeidiTran@users.noreply.github.com>
2018-12-05 09:07:13 -0500

    Create Chap7Ex1printFileContentInUppercase.py

---
+++ Chap7Ex1printFileContentInUppercase.py
@@ -0,0 +1,13 @@
+	# This program to read through a file and print the contents
+	# of the file (line by line) all in upper case.
+	
+	fname = input("Enter the file name: ")
+    try:
+        fhand = open(fname)
+    except:
+        print("File cannot be opened:", fname)
+        exit()
+    inp = str(fhand.read())
+    print(len(inp))
+    inp = inp.upper()
+    print(inp[:20])
