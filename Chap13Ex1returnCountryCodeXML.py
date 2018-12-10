cb1d2ea4d750749fcc09f8cf7bdef71f5b16c2e0
Thanh Giang Tran <43576846+HeidiTran@users.noreply.github.com>
2018-12-10 17:51:54 -0500

    Create Chap13Ex1returnCountryCodeXML.py

---
+++ Chap13Ex1returnCountryCodeXML.py
@@ -0,0 +1,41 @@
+import urllib.request, urllib.parse, urllib.error
+import xml.etree.ElementTree as ET
+import ssl
+
+api_key = False
+# If you have a Google Places API key, enter it here
+# api_key = 'AIzaSy___IDByT70'
+# https://developers.google.com/maps/documentation/geocoding/intro
+
+if api_key is False:
+    api_key = 42
+    serviceurl = 'http://py4e-data.dr-chuck.net/xml?'
+else :
+    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/xml?'
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+while True:
+    address = input('Enter location: ')
+    if len(address) < 1: break
+
+    parms = dict()
+    parms['address'] = address
+    if api_key is not False: parms['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(parms)
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url, context=ctx)
+
+    data = uh.read()
+    tree = ET.fromstring(data)
+
+    results = tree.findall('result')
+
+    if results[0].findall('address_component')[-1].findall('type')[0].text == 'country':
+        country_code = results[0].findall('address_component')[-1].find('short_name').text
+        print(country_code)
+    else:
+        print('This location is not in any country! Cannot return country code.')
