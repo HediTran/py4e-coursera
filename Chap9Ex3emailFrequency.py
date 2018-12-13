779fb7790b6f7e0aa2c404c4b0adf057ffc67692
Thanh Giang Tran <43576846+HeidiTran@users.noreply.github.com>
2018-12-13 13:04:57 -0500

    Create Chap9Ex3emailFrequency.py

---
+++ Chap9Ex3emailFrequency.py
@@ -0,0 +1,20 @@
+# This program read through a mail log, buid a histogram
+# to count how many messages have come from each email address
+
+fname = input("Enter the file name: ")
+try:
+    fhand = open(fname)
+except:
+    print("File cannot be opened:", fname)
+    exit()
+        
+email_freq = dict()
+        
+for line in fhand:
+    if line.startswith("From:"): continue
+    elif line.startswith("From"):
+        words = line.split()
+        email_freq[words[1]] = email_freq.get(words[1], 0) + 1
+
+for email in email_freq:
+    print(email, email_freq[email])
